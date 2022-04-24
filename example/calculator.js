a8acd77ea203ae18f891afb7fc45f2ab5b33c1f8
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-04-23 23:46:15 -0300

    feat: organise tests

---
+++ example/calculator.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  sum(x = 0, y = 0) {
+    return x + y;
+  },
+
+  mult(x = 0, y = 0) {
+    return x * y
+  },
+  
+}
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,17 +1,21 @@
-const app = require("../app")
+const calculator = require("../example/calculator")
 
-it("Should return the value 10 when pass args 5 and 5", function() {
+describe("Basic operations", function() {
 
-  let resuts = app.sum(5, 5)
-
-  expect(resuts).toEqual(10)
-
-})
-
-it("Should return the value 10 when pass args 2 and 5", function() {
-
-  let results = app.mult(2, 5)
-
-  expect(results).toEqual(10)
+  it("Should return the value 10 when pass args 5 and 5", function() {
   
+    let resuts = calculator.sum(5, 5)
+  
+    expect(resuts).toEqual(10)
+  
+  })
+  
+  it("Should return the value 10 when pass args 2 and 5", function() {
+  
+    let results = calculator.mult(2, 5)
+  
+    expect(results).toEqual(10)
+  
+  })
+
 })
